948d02372a38e1fdcab86b2872b44fbc44b49f4d
Dunnnan <777dunny@gmail.com>
2025-07-28 22:35:51 +0200

    Home view
    1. Added navbar fields for administrating the system and settings.
    
    2. Template of myReservations view.

--- src/main/java/com/dunnnan/reservations/controller/ReservationController.java
+++ src/main/java/com/dunnnan/reservations/controller/ReservationController.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.Optional;
@@ -47,6 +49,15 @@ public class ReservationController {
 
         redirectAttributes.addFlashAttribute("successMessage", "Reservation successful!");
         return "redirect:/resource/" + reservationDto.getResourceId();
+    }
+
+    @GetMapping("/reservations")
+    public String getReservations(
+            @RequestParam(name = "reservationId", required = false) String idOfReservationToBeDeleted,
+            Model model,
+            RedirectAttributes redirectAttributes
+    ) {
+        return "my-reservations";
     }
 
 }
