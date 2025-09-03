7527a6a9db02c274486a8696155b1c32f9090f6b
Dunnnan <777dunny@gmail.com>
2025-09-03 18:30:05 +0200

    chore(resourceType): fix app loading fail

--- src/main/java/com/dunnnan/reservations/service/ResourceTypeService.java
+++ src/main/java/com/dunnnan/reservations/service/ResourceTypeService.java
@@ -1,9 +1,5 @@
 package com.dunnnan.reservations.service;
 
-import com.dunnnan.reservations.repository.ResourceTypeRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
 //@Service
 //public class ResourceTypeService {
 //
